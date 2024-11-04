786d58c016a185f1bc94382f8fd6fc901b6fd36f
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2024-11-04 16:44:30 +0500

    add styles

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 85, 17, 202),
+                Color.fromARGB(255, 132, 76, 230)
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            child: const StartScreen()),
+      ),
+    ),
+  );
+}
